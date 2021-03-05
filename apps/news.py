68d02e680e19b67b156ffb92e30699e8cfeb9964
Davy Mitchell <daftspaniel@gmail.com>
2021-03-05 07:50:45 +0000

    News app - first version.

---
+++ apps/news.py
@@ -0,0 +1,14 @@
+import sqlite3
+import os.path
+import re
+from pathlib import Path
+
+newsboat_folder = Path.joinpath(Path.home(), '.newsboat')
+
+connection = sqlite3.connect(Path.joinpath(newsboat_folder, "cache.db"))
+cursor = connection.cursor()
+rows = cursor.execute("SELECT title,content,url FROM rss_item ORDER BY random() LIMIT 1").fetchall()
+text = re.sub('<[^<]+?>', '', rows[0][1]).replace('\n','').strip()
+print(rows[0][0][:78])
+print(text[:78])
+print(rows[0][2])
