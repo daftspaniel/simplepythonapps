3d81f20253bae3e96d9565f49664479affd5d683
Davy Mitchell <daftspaniel@gmail.com>
2021-04-09 07:27:29 +0100

    Pylinted news.py

--- apps/news.py
+++ apps/news.py
@@ -1,5 +1,7 @@
+"""
+    Prints out a random story from the newsboat database.
+"""
 import sqlite3
-import os.path
 import re
 from pathlib import Path
 
@@ -9,6 +11,7 @@ connection = sqlite3.connect(Path.joinpath(newsboat_folder, "cache.db"))
 cursor = connection.cursor()
 rows = cursor.execute("SELECT title,content,url FROM rss_item ORDER BY random() LIMIT 1").fetchall()
 text = re.sub('<[^<]+?>', '', rows[0][1]).replace('\n','').strip()
+
 print(rows[0][0][:78])
 print(text[:78])
 print(rows[0][2])
